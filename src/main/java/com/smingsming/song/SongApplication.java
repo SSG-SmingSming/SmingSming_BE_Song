4ff8c232e06d97a958cf048547e53a3702ca0aa9
jhy <yuns_97@naver.com>
2022-09-21 17:43:19 +0900

    song update application.yml

---
+++ src/main/java/com/smingsming/song/SongApplication.java
@@ -0,0 +1,13 @@
+package com.smingsming.song;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SongApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smingsming/song/SongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smingsming.song;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
