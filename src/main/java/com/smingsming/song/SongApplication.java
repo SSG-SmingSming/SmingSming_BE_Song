1ef33ddba8546de4d97de9373cf2a8f3384210d9
Bear-Sik <gs.min.1226@gmail.com>
2022-09-22 12:07:25 +0900

    add eureka client setting

--- src/main/java/com/smingsming/song/SongApplication.java
+++ src/main/java/com/smingsming/song/SongApplication.java
@@ -2,8 +2,10 @@ package com.smingsming.song;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class SongApplication {
 
 	public static void main(String[] args) {
