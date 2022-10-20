749bfab2bc05e721fcf74fea649c14046eb44f8b
Bear-Sik <108394795+Bear-Sik@users.noreply.github.com>
2022-10-20 16:04:22 +0900

    Add Search Ablum
    앨범 검색 기능 구현

--- src/main/java/com/smingsming/song/entity/artist/service/ArtistServicIempl.java
+++ src/main/java/com/smingsming/song/entity/artist/service/ArtistServicIempl.java
@@ -3,12 +3,19 @@ package com.smingsming.song.entity.artist.service;
 import com.smingsming.song.entity.artist.entity.ArtistEntity;
 import com.smingsming.song.entity.artist.vo.ArtistAddReqVo;
 import com.smingsming.song.entity.artist.repository.IArtistRepository;
+import com.smingsming.song.entity.artist.vo.ArtistVo;
+import com.smingsming.song.entity.playlist.vo.PlaylistLikesResVo;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -42,6 +49,25 @@ public class ArtistServicIempl implements IArtistService{
             return artist.get();
         else
             return null;
+    }
+
+
+    @Override
+    public List<ArtistVo> artistSearch(String keyword, int page) {
+
+        Pageable pr = PageRequest.of(page - 1, 20, Sort.by("id").descending());
+        keyword = keyword.strip();
+        List<ArtistEntity> artistList = iArtistRepository.findAllByNameContains(pr, keyword);
+
+        ModelMapper mapper = new ModelMapper();
+        List<ArtistVo> returnVo = new ArrayList<>();
+
+        artistList.forEach(v -> {
+            returnVo.add(mapper.map(v, ArtistVo.class));
+        });
+
+
+        return returnVo;
     }
 
     // 아티스트 정보수정
